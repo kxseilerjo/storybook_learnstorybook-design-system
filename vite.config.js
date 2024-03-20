c7cd19b03d117839b8348d52a692a8edaaf48caa
Jonas Seiler <jonas.seiler@kamax.com>
2024-03-20 14:13:29 +0100

    attempt fix of package bundle trying to reference

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import * as packageJson from './package.json';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
